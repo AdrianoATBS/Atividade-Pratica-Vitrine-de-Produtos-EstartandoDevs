5b8d2bde219c79cfbb0c25d3ef21fe507f117251
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2025-09-28 16:57:54 -0300

    feat: Adiciona botoes e funções de filtro por categoria

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,20 @@
 import { produtos } from "./dados_produtos";
 import CardProduto from "./components/CardProduto";
 import type { Produto } from "./components/CardProduto";
+import React from "react";
 
 function App() {
-
+    const [categoriaSelecionada, setCategoriaSelecionada] = React.useState<string>("Todos");
   return (
-    <div className="App">
-      {produtos.map((produto: Produto) => (
-        <CardProduto 
+    <div className="app">
+      <button onClick={() => setCategoriaSelecionada("Todos")}>Todos</button>
+      <button onClick={() => setCategoriaSelecionada("Smartphones")}>Smartphones</button>
+      <button onClick={() => setCategoriaSelecionada("Acessórios")}>Acessórios</button>
+      <button onClick={() => setCategoriaSelecionada("Notebooks")}>Notebooks</button>
+      {produtos.filter(produto => categoriaSelecionada === "Todos" || produto.categoria === 
+      categoriaSelecionada).map((produto: Produto) => (
+        
+        <CardProduto
           key={produto.id}
           produto={produto}
         />
