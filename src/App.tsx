c9fa5ca9e6f0c0c19b2ee079493c49bfa555aebe
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2025-09-28 17:09:14 -0300

    feat: Adiciona botão e função ordenar por preço

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,17 @@ import React from "react";
 
 function App() {
     const [categoriaSelecionada, setCategoriaSelecionada] = React.useState<string>("Todos");
+    const [ordenarPorPreco, setOrdenarPorPreco] = React.useState(false);
   return (
     <div className="app">
       <button onClick={() => setCategoriaSelecionada("Todos")}>Todos</button>
       <button onClick={() => setCategoriaSelecionada("Smartphones")}>Smartphones</button>
       <button onClick={() => setCategoriaSelecionada("Acessórios")}>Acessórios</button>
       <button onClick={() => setCategoriaSelecionada("Notebooks")}>Notebooks</button>
-      {produtos.filter(produto => categoriaSelecionada === "Todos" || produto.categoria === 
-      categoriaSelecionada).map((produto: Produto) => (
-        
+      <button onClick={() => setOrdenarPorPreco(!ordenarPorPreco)}>{ordenarPorPreco ? "Desordenar por preço" : "Ordenar por preço"}</button>
+      {produtos.filter(produto => categoriaSelecionada === "Todos" || produto.categoria === categoriaSelecionada)
+      .sort((a, b) => ordenarPorPreco ? a.preco - b.preco : 0)
+      .map((produto: Produto) => (
         <CardProduto
           key={produto.id}
           produto={produto}
