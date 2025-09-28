3a2b173dd0f646419ae29c4be548075900645ac5
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2025-09-28 16:50:42 -0300

    feat: Criado lógica de promoção e lógica de estoque

--- src/components/CardProduto.tsx
+++ src/components/CardProduto.tsx
@@ -5,6 +5,7 @@ export interface Produto {
   id: number;
   nome: string;
   preco: number;
+  precoPromocional?: number;
   imagemUrl: string;
   categoria: string;
   emEstoque?: boolean; 
@@ -18,13 +19,30 @@ export default function CardProduto({ produto }: CardProdutoProps) {
 
     return(
         <div>
-            <div className="card-produto">
-            <img src={produto.imagemUrl} alt={produto.nome}/>
-            <p className="categoria">{produto.categoria}</p>
-            <h2>{produto.nome}</h2>
-            <p className="preco">{produto.preco}</p>
-            <p className="estoque">{produto.emEstoque ? "Em estoque" : "Esgotado"}</p>
+            <div className={`card-produto ${produto.emEstoque? "" : 'esgotado'}`}>
+                {produto.precoPromocional && <span className="selo-promocao">Promoção!</span>}
+
+                <img src={produto.imagemUrl} alt={produto.nome} />
+                <p className="categoria">{produto.categoria}</p>
+                <h2>{produto.nome}</h2>
+
+                <div className="precos">
+                {produto.precoPromocional ?(
+                    <div >
+                        <p className="preco-antigo">{produto.preco}</p>
+                        <p className="preco-novo">{produto.precoPromocional}</p>
+                    </div>
+
+                ): (
+                    <p className="preco-novo">{produto.preco}</p>
+                )}
+                </div>
+                
+                <p>{produto.emEstoque ? "Em estoque" : "Esgotado"}</p>
+                <button disabled={!produto.emEstoque}>{produto.emEstoque ? "Adicionar ao carrinho" : "Produto Esgotado"}</button>
+            
             </div>
+
         </div>
     )
 }
